a616b5fbf2f78a2ad90f5f4972ed74d53f9128ef
Swanand Ingale <swanand19ingale@gmail.com>
2025-05-04 00:13:23 +0530

    initial commit. Just made some changes in proram.cs and added controllers and views.

---
+++ CarPoolingSystem/Controllers/RidesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarPoolingSystem.Controllers
+{
+    public class RidesController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- CarPoolingSystem/Program.cs
+++ CarPoolingSystem/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMvc();
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.Run();
