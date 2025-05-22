4ca806744b226a12efa043ec3e4c70a5279ea7d1
Swanand Ingale <swanand19ingale@gmail.com>
2025-05-22 14:53:30 +0530

    Added DB project to track Database changes

---
+++ CarPoolingDBProject/dbo/Tables/Users.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [UserId]       BIGINT         IDENTITY (1, 1) NOT NULL,
+    [FullName]     NVARCHAR (MAX) NULL,
+    [Email]        NVARCHAR (MAX) NULL,
+    [PhoneNumber]  NVARCHAR (25)  NULL,
+    [PasswordHash] NVARCHAR (MAX) NULL,
+    [CreatedAt]    DATETIME       DEFAULT (getdate()) NULL,
+    [IsDriver]     BIT            DEFAULT ((0)) NULL,
+    PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
+
---
+++ CarPoolingDBProject/dbo/Tables/Vehicles.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Vehicles] (
+    [VehicleId]      BIGINT        IDENTITY (1, 1) NOT NULL,
+    [VehicleNumber]  NVARCHAR (50) NOT NULL,
+    [Color]          NVARCHAR (70) NULL,
+    [SeatsAvailable] INT           NULL,
+    [CreatedAt]      DATETIME      DEFAULT (getdate()) NULL,
+    [UserId]         BIGINT        NULL,
+    [Model]          NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([VehicleId] ASC),
+    FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([UserId]),
+    UNIQUE NONCLUSTERED ([VehicleNumber] ASC)
+);
+
